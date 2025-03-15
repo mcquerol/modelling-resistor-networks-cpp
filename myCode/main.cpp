4ff65e2fd1063f27d37a0667ecaaf4c24f17a8b1
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-03-15 22:58:44 +0100

    created test for parallel connection

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -3,16 +3,16 @@
 
 #include "ResistorPart.h"
 #include "SeriesResistorConnection.h"
+#include "ParallelResistorConnection.h"
 #include "Resistor.h"
 using namespace std;
 
 int main ()
 {
 	/* test 1 Create a ResistorPart object */
-	ResistorPart r("R1", 42, 5);
+	ResistorPart r("R1", 1000, 5);
 	cout << r << endl;
 
-	cout << endl;
 	/* test 2 Series connection */
     std::shared_ptr<Resistor> r3 = std::make_shared<ResistorPart>("R3", 1000, 10);
     std::shared_ptr<Resistor> r4 = std::make_shared<ResistorPart>("R4", 4700, 10);
@@ -23,8 +23,23 @@ int main ()
     // Add R3 and R4 to the serial connection
     conn1 += r3;
     conn1 += r4;
-
     // Print the connection name
-    std::cout << conn1.getName() << std::endl;
+    cout << conn1.getName() << endl;
+    cout << conn1 <<endl;
+
+    /* test 3 Parallel connection */
+    std::shared_ptr<Resistor> r6 = std::make_shared<ResistorPart>("R6", 1000, 10);
+    std::shared_ptr<Resistor> r7 = std::make_shared<ResistorPart>("R7", 4700, 10);
+
+    // Create a serial resistor connection named R2
+    ParallelResistorConnection conn2("R5");
+
+    // Add R6 and R7 to the serial connection
+    conn2 += r6;
+    conn2 += r7;
+    // Print the connection name
+    cout << conn2.getName() << endl;
+    cout << conn2 << endl;
+
 	return 0;
 }
