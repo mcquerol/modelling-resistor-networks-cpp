a340e2bba9946f90e293e46f72d5b24fd53c642d
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-04-22 14:29:57 +0200

    closed #9 - implemented from method to parse line and return ResistorPtr

--- myCode/ResistorPart.cpp
+++ myCode/ResistorPart.cpp
@@ -7,6 +7,9 @@
 
 #include "ResistorPart.h"
 #include <string>
+#include <sstream>
+
+using namespace std;
 
 ResistorPart::ResistorPart(std::string name, float value, float tolerance) : Resistor{name}, value{value}, tolerance{tolerance}
 {
@@ -25,4 +28,22 @@ float ResistorPart::minimumValue() const
 float ResistorPart::maximumValue() const
 {
 	return value * (1 + tolerance);
+}
+
+ResistorPtr ResistorPart::from(std::string line)
+{
+    string text, nameStr, nominalValueStr, toleranceStr;
+    float nominalValue;
+    float tolerance;
+
+	istringstream iss(line);
+	getline(iss, nameStr, ';');
+	getline(iss, nominalValueStr, ';');
+	nominalValue = std::stof(nominalValueStr);
+	getline(iss, toleranceStr, '\n');
+	tolerance = std::stof(toleranceStr);
+
+	ResistorPart r(nameStr, nominalValue, tolerance);
+	ResistorPtr rPtr = std::make_shared<ResistorPart>(r);
+	return rPtr;
 }
